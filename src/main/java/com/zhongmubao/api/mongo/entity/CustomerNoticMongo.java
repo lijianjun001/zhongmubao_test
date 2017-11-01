4f14275ee1484ca9de1b8aaaced13837ade23bb1
Administrator <123@163.com>
2017-11-01 10:02:06 +0800

    分享  送 话费 未完成

---
+++ src/main/java/com/zhongmubao/api/mongo/entity/CustomerNoticMongo.java
@@ -0,0 +1,76 @@
+package com.zhongmubao.api.mongo.entity;
+
+import com.zhongmubao.api.mongo.entity.base.BaseModel;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+
+/**
+ * 公告通知
+ */
+@Document(collection = "CustomerNoticMongo")
+public class CustomerNoticMongo extends BaseModel {
+
+
+    @Field("Title")
+    private String title;
+    @Field("Content")
+    private String content;
+    @Field("EeginTime")
+    private Date beginTime;
+    @Field("EndTime")
+    private Date endTime;
+    @Field("CreateTime")
+    private Date createTime;
+    @Field("CustomerId")
+    private int customerId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getBeginTime() {
+        return beginTime;
+    }
+
+    public void setBeginTime(Date beginTime) {
+        this.beginTime = beginTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+}
