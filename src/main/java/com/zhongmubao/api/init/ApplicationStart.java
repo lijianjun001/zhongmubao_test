6c571ded7aacfc48979eb25fe5c78bbda1c4600a
Sunalong <286873958@qq.com>
2017-11-13 12:48:07 +0800

    修改应用初次启动init

--- src/main/java/com/zhongmubao/api/init/ApplicationStart.java
+++ src/main/java/com/zhongmubao/api/init/ApplicationStart.java
@@ -1,6 +1,5 @@
 package com.zhongmubao.api.init;
 
-import com.zhongmubao.api.entity.ExtActivityRecord;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
