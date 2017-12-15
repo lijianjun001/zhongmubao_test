cb7d9f856b8f27df5124aedfe20f2ca8af3d1d9d
mililin <mililin915>
2017-12-15 13:45:45 +0800

    model重新定义

--- src/main/java/com/zhongmubao/api/dto/response/my/transaction/TransactionDetailViewModel.java
+++ src/main/java/com/zhongmubao/api/dto/response/my/transaction/TransactionDetailViewModel.java
@@ -10,11 +10,8 @@ public class TransactionDetailViewModel {
     public TransactionDetailViewModel() {
     }
 
-    public TransactionDetailViewModel(String billType, WithdrawDetailModel withdrawDetailModel, BuySheepDetailModel buySheepDetailModel, RechargeDetailModel rechargeDetailModel) {
+    public TransactionDetailViewModel(String billType) {
         this.billType = billType;
-        this.withdrawDetailModel = withdrawDetailModel;
-        this.buySheepDetailModel = buySheepDetailModel;
-        this.rechargeDetailModel = rechargeDetailModel;
     }
 
     /**
@@ -32,64 +29,12 @@ public class TransactionDetailViewModel {
      */
     private String balance;
 
-    /**
-     * 充值详情
-     */
-    private RechargeDetailModel rechargeDetailModel;
-
-    /**
-     * 提现详情
-     */
-    private WithdrawDetailModel withdrawDetailModel;
-
-    /**
-     * 买羊详情
-     */
-    private BuySheepDetailModel buySheepDetailModel;
-
-    /**
-     * 赎回详情
-     */
-    private RedeemDetailModel redeemDetailModel;
-
     public String getBillType() {
         return billType;
     }
 
     public void setBillType(String billType) {
         this.billType = billType;
-    }
-
-    public WithdrawDetailModel getWithdrawDetailModel() {
-        return withdrawDetailModel;
-    }
-
-    public void setWithdrawDetailModel(WithdrawDetailModel withdrawDetailModel) {
-        this.withdrawDetailModel = withdrawDetailModel;
-    }
-
-    public BuySheepDetailModel getBuySheepDetailModel() {
-        return buySheepDetailModel;
-    }
-
-    public void setBuySheepDetailModel(BuySheepDetailModel buySheepDetailModel) {
-        this.buySheepDetailModel = buySheepDetailModel;
-    }
-
-    public RechargeDetailModel getRechargeDetailModel() {
-        return rechargeDetailModel;
-    }
-
-    public void setRechargeDetailModel(RechargeDetailModel rechargeDetailModel) {
-        this.rechargeDetailModel = rechargeDetailModel;
-    }
-
-    public RedeemDetailModel getRedeemDetailModel() {
-        return redeemDetailModel;
-    }
-
-    public void setRedeemDetailModel(RedeemDetailModel redeemDetailModel) {
-        this.redeemDetailModel = redeemDetailModel;
     }
 
     public String getTransactionDetail() {
